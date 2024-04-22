d796cf025b09b44e16326931771813fad3adf114
Pine <167698863+TechMgc@users.noreply.github.com>
2024-04-22 16:56:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,8 +17,8 @@ rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz
 rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-rootfs.img.gz
 rm -rf  bin/targets/x86/64/openwrt-x86-64-generic.manifest
 rm -rf  bin/targets/x86/64/profiles.json
-openwrt-efi=openwrt-x86-64-generic-squashfs-combined-efi.img.gz
-md5sum $openwrt-efi > openwrt-efi.md5
+openwrtEfi=openwrt-x86-64-generic-squashfs-combined-efi.img.gz
+md5sum $openwrtEfi > openwrt-efi.md5
 exit 0
 EOF
 
@@ -53,7 +53,7 @@ if [ -s  "/tmp/cloud_version" ]; then
 	#固件下载地址
 	new_version=`cat /tmp/cloud_version`
 	DOWNLOAD_URL=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt-x86-64-generic-squashfs-combined-efi.img.gz
-	openwrt-efi=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt-efi.md5
+	openwrtEfi=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt-efi.md5
 else
 	echo "无法获取到云端版本号！"
 	exit 1
@@ -61,7 +61,7 @@ fi
 #md5值验证，固件类型判断
 if [ "$current_version" != "$cloud_version" ];then
 	wget -P /tmp "$DOWNLOAD_URL" -O /tmp/openwrt-x86-64-generic-squashfs-combined-efi.img.gz
-	wget -P /tmp "$openwrt-efi" -O /tmp/openwrt-efi.md5
+	wget -P /tmp "$openwrtEfi" -O /tmp/openwrt-efi.md5
 	cd /tmp && md5sum -c openwrt-efi.md5
 		if [ $? != 0 ]; then
 			echo "您下载固件失败，请检查网络重试!"
