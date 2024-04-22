62bfa9a5a7290645a696e994f7ac4b24a580b09b
Pine <167698863+TechMgc@users.noreply.github.com>
2024-04-22 21:27:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -53,7 +53,7 @@ fi
 rm -f /tmp/cloud_version
 # 获取固件云端版本号、本地版本号信息
 current_version=`cat /etc/op_version`
-wget -qO- -t1 -T2 "https://api.github.com/repos/TechMgc/Openwrt/releases/latest" | grep "tag_name" | head -n 1 > /tmp/cloud_version
+wget -qO- -t1 -T2 "https://api.github.com/repos/TechMgc/Openwrt/releases/latest" | grep "tag_name" | cut -d '"' -f4 > /tmp/cloud_version
 if [ -s  "/tmp/cloud_version" ]; then
 	#固件下载地址
 	new_version=`cat /tmp/cloud_version`
