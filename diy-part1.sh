2cefe0df0444d91104565f2529f331472b3725a6
Pine <167698863+TechMgc@users.noreply.github.com>
2024-04-22 21:55:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -56,9 +56,9 @@ current_version=`cat /etc/op_version`
 wget -qO- -t1 -T2 "https://api.github.com/repos/TechMgc/Openwrt/releases/latest" | grep "tag_name" | cut -d '"' -f4 > /tmp/cloud_version
 if [ -s  "/tmp/cloud_version" ]; then
 	#固件下载地址
-	new_version=`cat /tmp/cloud_version`
-	DOWNLOAD_URL=https://github.com/TechMgc/Openwrt/releases/download/${new_version}/openwrt-x86-64-generic-squashfs-combined-efi.img.gz
-	openwrtEfi=https://github.com/TechMgc/Openwrt/releases/download/${new_version}/openwrt-efi.md5
+	cloud_version=`cat /tmp/cloud_version`
+	DOWNLOAD_URL=https://github.com/TechMgc/Openwrt/releases/download/${cloud_version}/openwrt-x86-64-generic-squashfs-combined-efi.img.gz
+	openwrtEfi=https://github.com/TechMgc/Openwrt/releases/download/${cloud_version}/openwrt-efi.md5
 else
 	echo "无法获取到云端版本号！"
 	exit 1
